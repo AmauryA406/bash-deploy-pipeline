ee8fcbe2ffc88e1dc97934b4de575e0f7815a696
Amaury Allemand <amauryallemand8@gmail.com>
2026-03-08 17:32:23 +0000

    feat: ajout pipeline.sh

---
+++ scripts/pipeline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+COMMAND=$1
+
+if [ -z "$COMMAND" ]; then
+echo "Usage: ./pipeline.sh {build|test|deploy|rollback|logs}"
+exit 1
+fi
+
+build() {
+echo "build"
+}
+
+test() {
+echo "test"
+}
+
+deploy() {
+echo "deploy"
+}
+
+rollback() {
+echo "rollback"
+}
+
+logs() {
+echo "logs"
+}
+
+case $COMMAND in
+"build")
+build
+;;
+"test")
+test
+;;
+"deploy")
+deploy
+;;
+"rollback")
+rollback
+;;
+"logs")
+logs
+;;
+*)
+echo "commande inconnue : $COMMAND"
+;;
+esac
